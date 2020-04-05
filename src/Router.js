41fce8af0157e9f8c44c3f44c0c77a414f8caeb3
Ramanpreet Singh <ramanpreet230@gmail.com>
2020-04-05 13:42:48 -0400

    Fixes in router.

--- src/Router.js
+++ src/Router.js
@@ -11,16 +11,31 @@ import {
     Signup,
 } from './screens/index';
 
-const RouterComponent = () => {
-    console.log('is cllaed...');
-    
+const RouterComponent = () => {    
     return (
       <Router>
       <Scene key="root">
-         <Scene key = "splash" component = {Splash} title = "Splash" initial = {true} hideNavBar={true}/>
-         <Scene key = "Login" component = {Login} title = "Login" hideNavBar={true} gestureEnable={false}
-      panHandlers={null}/>
-         <Scene key = "Signup" component = {Signup} title = "Signup" hideNavBar={true}/>
+         <Scene 
+         key = "splash" 
+         component = {Splash} 
+         title = "Splash" 
+         initial = {true} 
+         hideNavBar={true}
+         />
+         <Scene 
+         key = "Login" 
+         component = {Login} 
+         title = "Login" 
+         hideNavBar={true} 
+         gestureEnable={false}
+         panHandlers={null}
+         />
+         <Scene 
+         key = "Signup" 
+         component = {Signup} 
+         title = "Signup" 
+         hideNavBar={true}
+         />
       </Scene>
    </Router>
     );
