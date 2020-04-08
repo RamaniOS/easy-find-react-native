87aea39cad1a61dac16c2d22de0a68eee76e8fbd
Ramanpreet Singh <ramanpreet230@gmail.com>
2020-04-08 19:18:06 -0400

    Working on UI of detail screen.

--- src/screens/Detail/Detail.js
+++ src/screens/Detail/Detail.js
@@ -2,29 +2,61 @@
 Detail screen
 */
 
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 import {
   View,
   Text,
   TouchableOpacity,
-  SafeAreaView,
-  StatusBar
+  Image,
+  SafeAreaView
 } from 'react-native'
 import styles from './DetailStyle'
-import { Input, Button, TitleView, RestaurantList } from '../../components'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
-import { password_regex, user_name_regex } from '../../Utilis'
-import * as Constants from '../../Constants'
+import { Rating } from 'react-native-elements';
+import { Images } from '../../theme'
+import { Actions } from 'react-native-router-flux';
+import { ScrollView } from 'react-native-gesture-handler';
 
 class Detail extends Component {
+
+  backButtonClicked() {
+    Actions.pop()
+  }
 
   // Render UI objects 
   render() {
     return (
       <>
-        <View style={styles.mainConatiner}>
-          
-        </View>
+        <ScrollView bounces={false}>
+          <View style={styles.mainConatiner}>
+            <View style={styles.topContainer}>
+              <SafeAreaView></SafeAreaView>
+              <TouchableOpacity style={styles.backButton} onPress={() => { this.backButtonClicked() }}>
+                <Image source={Images.back} />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.bottomContainer}>
+              <View style={styles.infoRowStyle}>
+                <View style={styles.titleContainer}>
+                  <Text style={styles.nameStyle}>Banjara villa</Text>
+                </View>
+                <View style={styles.ratingContainer}>
+                  <Rating
+                    readonly
+                    type='star'
+                    ratingCount={5}
+                    imageSize={20}
+                    style={styles.ratingStyle}
+                  />
+                  <Text style={styles.reviewStyle}>2300 Reviews</Text>
+                </View>
+                <Text style={styles.mealsStyle}>Tea, sancks, meat</Text>
+                <Text style={styles.priceStyle}>$$$</Text>
+                <Text style={styles.addressStyle}>12 Danum Rd, Brampton</Text>
+              </View>
+            </View>
+          </View>
+        </ScrollView>
       </>
     )
   }
--- src/screens/Detail/DetailStyle.js
+++ src/screens/Detail/DetailStyle.js
@@ -5,9 +5,67 @@ Style for Detail screen
 import {StyleSheet} from 'react-native'
 import {Fonts, Colors} from '../../theme'
 
+
 export default StyleSheet.create({
     mainConatiner: {
         flex: 1,
-        backgroundColor: Colors.white
+    },
+    topContainer: {
+        height: 250,
+        backgroundColor: 'pink'
+    },
+    backButton: {
+        alignSelf: "flex-start",
+        top: '15%',
+        left: 20,
+        width: 50,
+        height: 50
+    },
+    bottomContainer: {
+        flex: 1,
+    },
+    infoRowStyle: {
+        flex: 1,
+        padding: 20
+    },
+    titleContainer: {
+        flexDirection: 'row',
+    },
+    nameStyle: {
+        fontFamily: Fonts.font_bold,
+        fontSize: 22,
+        color: Colors.black,
+    },
+    ratingContainer: {
+        marginVertical: 2,
+        flexDirection: 'row',
+    },
+    ratingStyle: {
+        marginVertical: 5
+    },
+    reviewStyle: {
+        fontFamily: Fonts.font_regular,
+        fontSize: 15,
+        marginHorizontal: 14,
+        alignSelf: 'center',
+        marginVertical: 7
+    },
+    mealsStyle: {
+        fontFamily: Fonts.font_regular,
+        fontSize: 16,
+        color: Colors.black,
+        marginVertical: 2
+    },
+    priceStyle: {
+        fontFamily: Fonts.font_regular,
+        fontSize: 16,
+        color: Colors.black,
+        marginVertical: 2
+    },
+    addressStyle: {
+        fontFamily: Fonts.font_light,
+        fontSize: 14,
+        color: Colors.black,
+        marginVertical: 2
     }
 })
